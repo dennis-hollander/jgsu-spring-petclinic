ddbd62c02c6aa87d41581e21a3ceb6b57927d3cf
Dennis <dennis.hollander@thenextgen.nl>
2022-09-13 17:01:01 +0200

    reverting changes in broken test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("dennis");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
